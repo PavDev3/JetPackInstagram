334a18ccf8c4b5123154c6ddf1d3a10f60418ba9
Pablo Nuñez <pnunfe@gmail.com>
2023-03-09 21:07:59 +0000

    Login Response and client implemented

---
+++ app/src/main/java/com/example/jetpackinstagram/login/data/network/response/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.example.jetpackinstagram.login.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(@SerializedName("success") val success:Boolean)
